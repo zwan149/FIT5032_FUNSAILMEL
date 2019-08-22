46a1d4759ec8595fadfe9f33f1e425da0821bf11
kevin <zwan149@student.monash.edu>
2019-08-23 09:47:15 +1000

    Create Assignment Project

---
+++ FIT5032_FUNSAILMEL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_FUNSAILMEL.Startup))]
+namespace FIT5032_FUNSAILMEL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
